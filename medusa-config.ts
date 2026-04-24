874c8f332e5309c1e8c98361beacdd1922f6bee7
mushfique294 <mushfique.technext@gmail.com>
2026-04-24 17:11:30 +0600

    fix: medusa config file update

--- medusa-config.ts
+++ medusa-config.ts
@@ -4,11 +4,11 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
+    databaseUrl: process.env.DATABASE_URL || "postgresql://medusa:medusa@postgres:5432/medusa",
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors: process.env.STORE_CORS || "*",
+      adminCors: process.env.ADMIN_CORS || "*",
+      authCors: process.env.AUTH_CORS || "*",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
